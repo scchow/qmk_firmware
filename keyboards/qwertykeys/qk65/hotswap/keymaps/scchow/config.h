6b61841f4f8009d1bb06a3680ec8507c80b3a326
Scott Chow <11098209+scchow@users.noreply.github.com>
2022-12-23 16:33:26 -0800

    Added QK65 Keymapping

---
+++ keyboards/qwertykeys/qk65/hotswap/keymaps/scchow/config.h
@@ -0,0 +1,25 @@
+/* Copyright 2021 scchow
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#pragma once
+// Tapping toggle settings
+
+// Number of times to tap a modifier key to toggle
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
+#undef TAPPING_TERM
+#define TAPPING_TERM 175
