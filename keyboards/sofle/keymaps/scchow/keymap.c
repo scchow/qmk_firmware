04a3b666d326bb6949e3a351a537f3f462415769
Scott Chow <11098209+scchow@users.noreply.github.com>
2022-12-24 11:57:13 -0800

    Refactoring folder structure for Sofle

--- keyboards/sofle/keymaps/scchow/keymap.c
+++ keyboards/sofle/keymaps/scchow/keymap.c
@@ -21,150 +21,9 @@ Copied from rbg_default/keymap.c
 Applied modifications based on: https://github.com/josefadamcik/SofleKeyboard/pull/90#issuecomment-999033489
 */
 
-#include <stdio.h>
-
 #include QMK_KEYBOARD_H
 
-#define INDICATOR_BRIGHTNESS 30
-#define HSV_OVERRIDE_HELP(h, s, v, Override) h, s , Override
-#define HSV_OVERRIDE(hsv, Override) HSV_OVERRIDE_HELP(hsv,Override)
-
-
-/* LED Indices for Sofle RGB v3.0
- *
- * Front
- *                         LEFT SIDE                                                      RIGHT SIDE
- *                                                       NOTE: added a +1 offset to values below so that it works, idk why it's needed)
- *                 Column Number
- *                  0     1      2      3      4     5       6          13      12     11     10     9      8       7
- * Row Num    ,-----------------------------------------.                    ,-----------------------------------------.
- *    0       |  11  |  12  |  21  |  22  |  31  |  32  |                    |  68  |  67  |  58  |  57  |  48  |  47  |
- *            |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- *    1       |  10  |  13  |  20  |  23  |  30  |  33  |                    |  69  |  66  |  59  |  56  |  49  |  46  |
- *            |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- *    2       |  9   |  14  |  19  |  24  |  29  |  34  |-------.    ,-------|  70  |  65  |  60  |  55  |  50  |  45  |
- *            |------+------+------+------+------+------|   0   |    |   36   |------+------+------+------+------+-----|
- *    3       |  8   |  15  |  18  |  25  |  28  |  35  |-------|    |-------|  71  |  64  |  61  |  54  |  51  |  44  |
- *            `-----------------------------------------/       /    \      \-----------------------------------------'
- *    4              |  7   |  17  |  18  |  26  |     /   27  /      \  63  \      |  62  |  53  |  52  |   43  |
- *                   |      |      |      |      |    /       /        \      \     |      |      |      |      |
- *                     `-------------------------------------'          '------''-------------------------------'
- * Back underglow
- *                          RIGHT SIDE                                                    LEFT SIDE
- *  NOTE: added a +1 offset to values below so that it works, idk why it's needed)
- *
- * Column Num    0     1      2      3      4     5       6          13      12     11     10     9      8       7
- *   Row Num  ,-----------------------------------------.                    ,-----------------------------------------.
- *      0     |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- *            |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- *      1     |      |  37  |      | 38   |      |  39  |                    |  3  |      |  2  |      |  1  |      |
- *            |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- *      2     |      |      |      |      |      |      |-------.    ,-------|      |      |      |      |      |      |
- *            |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- *      3     |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
- *            `-----------------------------------------/       /     \      \-----------------------------------------'
- *      4            | 42   |   41 |      |      |   40/       /       \      \ 4  |      |   5 |      |   6 |
- *                   |      |      |      |      |    /       /         \      \    |      |      |      |      |
- *                   `---------------------------------------'           '------''------------------------------'
- */
-
-
-// Number of Indicator Lights
-#define NUM_INDICATORS 1
-// Number of underglow LEDs
-#define NUM_UNDERGLOW 6
-// Number of Per-Key RGB Backlighting
-#define NUM_BACKLIGHT 29
-// Number of LEDs in each half of board
-#define NUM_PER_SIDE NUM_INDICATORS + NUM_UNDERGLOW + NUM_BACKLIGHT + 1 // 1+6+29 +1 = 36 + 1 = 37 (I am not sure why there is an extra +1 offset for leds on right side)
-// Number of outer column keys
-#define NUM_OUTER_FN_KEYS 4
-
-// Starting index of underglow lighting
-#define START_UNDERGLOW 1
-// Starting LED number of all Per-Key RGB Backlighting  (should match key at front: (row4, col0))
-#define START_PER_KEY_RGB NUM_INDICATORS + NUM_UNDERGLOW  // 1 + 6 = 7
-// Starting index of outer column keys (Columns 0 and 7) (number of key at (row3, col7))
-#define START_OUTER_FN_KEYS 8 // 7+1 = 9; Offset the outer thumb row key
-
-// Light combinations
-// Set the LEDs for the indicator leds
-#define SET_INDICATORS(hsv) \
-    {0, NUM_INDICATORS, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {NUM_PER_SIDE, NUM_INDICATORS, hsv}
-
-// Set the LEDs for the underglow leds
-#define SET_UNDERGLOW(hsv) \
-	{START_UNDERGLOW, NUM_UNDERGLOW, hsv}, \
-    {NUM_PER_SIDE+START_UNDERGLOW, NUM_UNDERGLOW, hsv}
-
-// Set the LEDs for the underglow leds
-// TODO: Check these because they might be incorrect
-#define SET_NUMPAD(hsv)     \
-	{NUM_PER_SIDE+16, 5, hsv},\
-	{NUM_PER_SIDE+25, 3, hsv},\
-	{NUM_PER_SIDE+28, 3, hsv}
-
-// Set the LEDs for the numrow leds
-#define SET_NUMROW(hsv) \
-	{11, 2, hsv}, \
-	{21, 2, hsv}, \
-	{31, 2, hsv}, \
-	{NUM_PER_SIDE+ 11, 2, hsv}, \
-	{NUM_PER_SIDE+ 21, 2, hsv}, \
-	{NUM_PER_SIDE+ 31, 2, hsv}
-
-/*
-// Set the LEDs for the outside column (columns 0 and 7)
-#define SET_OUTER_COL(hsv) \
-	{START_OUTER_FN_KEYS, NUM_OUTER_FN_KEYS, hsv},\
-	  {NUM_PER_SIDE + START_OUTER_FN_KEYS, NUM_OUTER_FN_KEYS, hsv}
-*/
-
-// Set the LEDs for the thumb cluster (only 2 keys near knob)
-#define SET_THUMB_CLUSTER(hsv) 	\
-    {26, 2, hsv}
-    // {NUM_PER_SIDE + 26, 2, hsv}
-
-// Set the LEDs for the thumb cluster
-#define SET_THUMB_CLUSTER_FULL(hsv) 	\
-    {7, 1, hsv},\
-    {16, 2, hsv},\
-    {26, 2, hsv}
-    // {NUM_PER_SIDE + 7, 1, hsv},
-    // {NUM_PER_SIDE + 16, 2, hsv},
-    // {NUM_PER_SIDE + 26, 2, hsv}
-
-// Set all LEDs outside of alpha keys
-#define SET_LAYER_ID(hsv) 	\
-	{0, NUM_INDICATORS, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-	{START_UNDERGLOW, NUM_UNDERGLOW, hsv}, \
-	{START_OUTER_FN_KEYS, NUM_OUTER_FN_KEYS, hsv}, \
-    {7, 1, hsv},\
-    {16, 2, hsv},\
-    {26, 2, hsv},\
-      {NUM_PER_SIDE+0, NUM_INDICATORS, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-      {NUM_PER_SIDE+START_UNDERGLOW, NUM_UNDERGLOW, hsv},\
-	  {NUM_PER_SIDE + START_OUTER_FN_KEYS, NUM_OUTER_FN_KEYS, hsv},\
-    {NUM_PER_SIDE + 7, 1, hsv},\
-    {NUM_PER_SIDE + 16, 2, hsv},\
-    {NUM_PER_SIDE + 26, 2, hsv}
-
-enum sofle_layers {
-    _DEFAULTS = 0,
-    _BASE = 0,
-    _NAV,
-    _NAV2,
-    _GAME,
-    _GAME2,
-    _NUMPAD,
-    _SWITCH
-};
-
-enum custom_keycodes {
-    KC_BASE = SAFE_RANGE,
-    KC_D_MUTE
-};
+#include "enums.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -334,159 +193,63 @@ LT(_GAME2,KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,              
 )
 };
 
+#ifdef RGBLIGHT_ENABLE
+    #include "rgb.h"
+#endif
+
+#ifdef OLED_ENABLE
+    #include "oled.h"
+#endif
+
+#ifdef ENCODER_ENABLE
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    // Left encoder controls volume
+    if (index == 0){
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        }
+        else{
+            tap_code(KC_VOLD);
+        }
+		}
+    // Right encoder scrolls (on all layers)
+    else if (index == 1) {
+        // switch (get_highest_layer(layer_state)) {
+            // case _BASE:
+            //     break;
+            // case _BASE:
+            // 		if (clockwise) {
+            // 			tap_code(KC_WH_D);
+            // 		} else {
+            // 			tap_code(KC_WH_U);
+            // 		}
+            // 	break;
+            // default:
+                if (clockwise) {
+                    tap_code(KC_WH_D);
+                } else {
+                    tap_code(KC_WH_U);
+                }
+                // break;
+        // }
+    }
+    return true;
+}
+
+#endif
+
+
 layer_state_t layer_state_set_user(layer_state_t state) {
 
     // state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 
     #ifdef RGBLIGHT_ENABLE
-	rgblight_set_layer_state(0, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_BASE));
-	rgblight_set_layer_state(1, layer_state_cmp(state, _NAV));
-	rgblight_set_layer_state(2, layer_state_cmp(state, _NAV2));
-	rgblight_set_layer_state(3, layer_state_cmp(state, _GAME));
-	rgblight_set_layer_state(5, layer_state_cmp(state, _GAME2));
-	rgblight_set_layer_state(4, layer_state_cmp(state, _NUMPAD));
-	// rgblight_set_layer_state(5, layer_state_cmp(state, _SWITCH));
+        rgblight_set_based_on_layer(state);
     #endif
     return state;
 }
 
-#ifdef RGBLIGHT_ENABLE
-char layer_state_str[70];
-// Now define the array of layers. Later layers take precedence
 
-// QWERTY,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_qwerty_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-  SET_LAYER_ID(HSV_RED)
-
-);
-
-// _NUM,
-// Light on outer column and underglow
-const rgblight_segment_t PROGMEM layer_num_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-	SET_LAYER_ID(HSV_TEAL)
-
-);
-// _SYMBOL,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_symbol_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-	SET_LAYER_ID(HSV_BLUE)
-
-    );
-// _COMMAND,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_command_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-  SET_LAYER_ID(HSV_PURPLE)
-);
-
-//_NUMPAD
-const rgblight_segment_t PROGMEM layer_numpad_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-	SET_INDICATORS(HSV_ORANGE),
-    SET_UNDERGLOW(HSV_ORANGE),
-	SET_NUMPAD(HSV_BLUE),
-    SET_THUMB_CLUSTER(HSV_ORANGE)
-    );
-// _SWITCHER   // light up top row
-const rgblight_segment_t PROGMEM layer_switcher_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-	SET_LAYER_ID(HSV_GREEN),
-	SET_NUMROW(HSV_GREEN)
-);
-
-const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    layer_qwerty_lights,
-	layer_num_lights,// overrides layer 1
-	layer_symbol_lights,
-    layer_command_lights,
-	layer_numpad_lights,
-	layer_switcher_lights  // Overrides other layers
-);
-
-// layer_state_t layer_state_set_user(layer_state_t state) {
-// 	rgblight_set_layer_state(0, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_BASE));
-// 	rgblight_set_layer_state(1, layer_state_cmp(state, _LOWER));
-// 	rgblight_set_layer_state(2, layer_state_cmp(state, _RAISE));
-// 	rgblight_set_layer_state(3, layer_state_cmp(state, _ADJUST));
-// 	rgblight_set_layer_state(4, layer_state_cmp(state, _NUMPAD));
-// 	rgblight_set_layer_state(5, layer_state_cmp(state, _SWITCH));
-//     return state;
-// }
-void keyboard_post_init_user(void) {
-    // Enable the LED layers
-    rgblight_layers = my_rgb_layers;
-
-	rgblight_mode(10);// haven't found a way to set this in a more useful way
-
-}
-#endif
-
-#ifdef OLED_ENABLE
-#include "bongocat.h"
-
-// int/String for holding WPM
-uint8_t wpm_int = 0;
-char wpm_str[4] = {'0', '0', '0', '\0'};
-
-const PROGMEM char *wpm_status_str = PSTR("\n\nScottChow\n\n\nWPM\n\n");
-const PROGMEM char *layer_status_str = PSTR("\n\nLAYER\n");
-
-static void print_status_narrow(uint8_t wpm){
-    // Print status message
-    oled_write_P(wpm_status_str, false);
-
-    // Safer wpm printing from:
-    //https://github.com/saikocat/keebs/blob/master/docs/qmk_missing_guide/oled.md#wpm-display-optimization
-    wpm_str[0] = '0' + wpm / 100; // hundred's digit place
-    wpm_str[1] = '0' + (wpm / 10) % 10; // ten's digit place
-    wpm_str[2] = '0' + wpm % 10; // single digit place
-    oled_write_ln(wpm_str, false);
-
-    // Print current layer
-    oled_write_P(layer_status_str, false);
-    switch (get_highest_layer(layer_state)) {
-        case _BASE:
-            oled_write_ln("Base", false);
-            break;
-        case _NAV:
-            oled_write_ln("Nav", false);
-            break;
-        case _NAV2:
-            oled_write_ln("Nav2", false);
-            break;
-        case _GAME:
-            oled_write_ln("Game", false);
-            break;
-        case _GAME2:
-            oled_write_ln("Game2", false);
-            break;
-        case _NUMPAD:
-            oled_write_ln("Nump", false);
-            break;
-        default:
-            oled_write_ln("Undef", false);
-    }
-}
-
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (is_keyboard_master()) {
-        return OLED_ROTATION_270;
-    }
-    return rotation;
-}
-
-bool oled_task_user(void) {
-    wpm_int = get_current_wpm();
-    // render status on master
-    if (is_keyboard_master()) {
-        print_status_narrow(wpm_int);
-    }
-    // render logo on secondary
-    else {
-        render_anim(wpm_int);
-    }
-    return false;
-}
-
-#endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -537,40 +300,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-#ifdef ENCODER_ENABLE
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    // Left encoder controls volume
-    if (index == 0){
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        }
-        else{
-            tap_code(KC_VOLD);
-        }
-		}
-    // Right encoder scrolls (on all layers)
-    else if (index == 1) {
-        // switch (get_highest_layer(layer_state)) {
-            // case _BASE:
-            //     break;
-            // case _BASE:
-            // 		if (clockwise) {
-            // 			tap_code(KC_WH_D);
-            // 		} else {
-            // 			tap_code(KC_WH_U);
-            // 		}
-            // 	break;
-            // default:
-                if (clockwise) {
-                    tap_code(KC_WH_D);
-                } else {
-                    tap_code(KC_WH_U);
-                }
-                // break;
-        // }
-    }
-    return true;
-}
-
-#endif
