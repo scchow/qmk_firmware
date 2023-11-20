100a081f4bfcedc62e0f42a4cf088b2cadc2fb95
Scott Chow <11098209+scchow@users.noreply.github.com>
2023-11-19 16:10:26 -0800

    More edits to keymaps

--- keyboards/sofle/keymaps/scchow/headers/layers.h
+++ keyboards/sofle/keymaps/scchow/headers/layers.h
@@ -27,9 +27,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|---------------+-------+--------+--------+-------+------|  |--------+-------+--------+--------+--------+---------|
   LT(_NAV2, KC_ESC),  KC_A,   KC_S,   KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //|------+-------+--------+--------+--------+------|  ===  |  |  ===  |--------+-------+--------+--------+--------+---------|
-  KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  QK_BOOT,    QK_BOOT, KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_DEL,
+  KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  QK_BOOT,    QK_BOOT, KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
   //|------+-------+--------+--------+--------+------|  ==  |  |  ===  |--------+-------+----------+--------+--------+---------|
-   KC_LALT, TT(_NAV), KC_LGUI, LT(_NAV, KC_SPC), MT(MOD_LCTL, KC_BSPC), MT(MOD_LALT, KC_DEL),  LT(_NAV, KC_SPC),  KC_ENT,  TT(_NAV), TG(_GAME)
+   KC_LALT, TT(_NAV), KC_LGUI, LT(_NAV, KC_SPC), MT(MOD_LCTL, KC_BSPC), MT(MOD_LALT, KC_DEL),  LT(_NAV, KC_SPC),  MT(MOD_LSFT,KC_ENT),  TT(_NAV), TG(_GAME)
   //    \-------+--------+---------+-------+-----------------|  |-------+--------+--------+---------+-------/
 ),
 
